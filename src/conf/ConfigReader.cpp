6d01060044b65bb34dc426a6f05737cd0c2556f7
Heersin <teablearcher@gmail.com>
2021-01-28 12:03:21 +0800

    config parser done

--- src/conf/ConfigReader.cpp
+++ src/conf/ConfigReader.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "ConfigReader.h"
+
+ConfigReader::ConfigReader(string &conf_path) : reader(conf_path)
+{
+    if (reader.ParseError() < 0)
+    {
+        cerr << "can't load init file" << endl;
+        exit(-1);
+    }
+}
+
+bool ConfigReader::getBool(string &section, string &keyname)
+{
+    return reader.GetBoolean(section, keyname, false);
+}
+
+long ConfigReader::getInt(string &section, string &keyname)
+{
+    return reader.GetInteger(section, keyname, -1);
+}
+
+double ConfigReader::getReal(string &section, string &keyname)
+{
+    return reader.GetReal(section, keyname, -1);
+}
+
+string ConfigReader::getString(string &section, string &keyname)
+{
+    return reader.Get(section, keyname, "UNKNOWN");
+}
--- src/conf/ConfigReader.h
+++ src/conf/ConfigReader.h
@@ -5,9 +5,21 @@
 #ifndef SWARMRAY_CONFIGREADER_H
 #define SWARMRAY_CONFIGREADER_H
 
+#include <iostream>
+#include <cstring>
+#include "../libs/inih/INIReader.h"
+using namespace std;
 
 class ConfigReader {
+public:
+    explicit ConfigReader(string &conf_path);
+    string getString(string &section, string &keyname);
+    long getInt(string &section, string &keyname);
+    double getReal(string &section, string &keyname);
+    bool getBool(string &section, string &keyname);
 
+private:
+    INIReader reader;
 };
 
 
