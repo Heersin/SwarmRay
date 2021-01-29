ebab3702d25cc87efe676db05b4c84661c0693a2
Heersin <teablearcher@gmail.com>
2021-01-29 11:22:23 +0800

    build easylogging

---
+++ src/log/ezlog.cpp
@@ -0,0 +1,9 @@
+//
+// Created by heersin on 1/29/21.
+//
+
+#include "ezlog.h"
+void start_logging(int argc, char *argv[])
+{
+    START_EASYLOGGINGPP(argc, argv);
+}
---
+++ src/log/ezlog.h
@@ -0,0 +1,12 @@
+//
+// Created by heersin on 1/29/21.
+//
+
+#ifndef SWARMRAY_EZLOG_H
+#define SWARMRAY_EZLOG_H
+
+#include "../libs/easyloggingpp/easylogging++.h"
+
+void start_logging(int argc, char *argv)
+
+#endif //SWARMRAY_EZLOG_H
